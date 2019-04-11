b80812badccc44cfa62a407efea572d81986cdd3
kylemath <kylemath@gmail.com>
2019-04-11 15:58:53 -0600

    group analysis code, cleaned up data folder, makes figures

--- group_analysis.m
+++ group_analysis.m
@@ -1,22 +1,51 @@
 
-sub_nums = {'001', '002'}
-nsubs = length(sub_nums)
+sub_nums = {'001', '002', '003', '004', '005', '006', '007'};
+nsubs = length(sub_nums);
+
+figure('Position',[25,25,1000,1000]); 
+widthHeight = ceil(sqrt(nsubs));
+
+
 
 for i_sub = 1:nsubs 
-	%Find output filename
+	current_sub = sub_nums{i_sub};
 
-	Filename = dir(['.\Data\' sub_nums(i_sub) '*')
+	%Find output filename
+	Filename = dir(['.\Data\' current_sub '*']);
 
 	%% Save data
-	load(['.\Data\' Filename])
+	load(['.\Data\' Filename.name]);
 
-	% /////////////////////////////////////////////////////////////////////////
+	if i_sub == 1 % make output variables here once loaded first file
+		turn_group = zeros(nsubs,length(soas));
+		control_group = zeros(nsubs,length(soas));
+	end
+
 	%% Plot results
-	figure; 
-	plot(soas,turn_out,'r',soas,control_out,'b'); 
+	subplot(widthHeight,widthHeight,i_sub); 
+		plot(soas,turn_out,'r',soas,control_out,'b'); 
+			legend({'Flexion','Control'});
+			xlim([min(soas) max(soas)]); 
+			set(gca,'XTick',min(soas):1:max(soas))
+			xlabel('Gabor First < -- SOA (frames) -- > Gabor After')
+			ylabel('Detection Proportion')
+			ylim([.01 1.05])
+			title(current_sub)
+
+	turn_group(i_sub,:) = turn_out;
+	control_group(i_sub,:) = control_out;
+
+end
+
+
+%% Plot Grand Average results
+figure;
+	boundedline(soas, mean(turn_group,1), std(turn_group,[],1) / sqrt(nsubs),'r', ...
+		soas, mean(control_group,1), std(control_group,[],1) / sqrt(nsubs),'b');
 	legend({'Flexion','Control'});
-	xlim([min(soas) max(soas)]); xticks(min(soas):1:max(soas))
+	xlim([min(soas) max(soas)]); 
+	set(gca,'XTick',min(soas):1:max(soas))
 	xlabel('Gabor Change First < ------ SOA (frames) ------ > Gabor Change After')
 	ylabel('Detection Proportion')
 	ylim([.01 1.05])
-end
+	title('Grand Average')
