b5e7136b20cb33ba4e41b1b415f08a07815bcacc
ssheldo <sssheldo@gmail.com>
2019-05-04 18:08:58 -0600

    RT & angle

--- detail_analysis.m
+++ detail_analysis.m
@@ -115,5 +115,73 @@ figure;
 	ylabel('Reaction Time')
 % 	ylim([.01 1.05])
 	title('Grand Average')
+    
+    
+       
+%% Accuracy by angle
+%out_angle: 270 left, 90 Right, 0 straight 
+figure('Position',[25,25,1000,1000]); 
+widthHeight = ceil(sqrt(nsubs));
+for i_sub = 1:nsubs 
+	current_sub = sub_nums{i_sub};
+
+	%Find output filename
+	Filename = dir(['.\Data\' current_sub '*']);
+
+	% Save data
+	load(['.\Data\' Filename.name]);
+
+    if i_sub == 1 % make output variables here once loaded first file
+		turn_groupL = zeros(nsubs,length(soas));
+        turn_groupR = zeros(nsubs,length(soas));
+		control_group = zeros(nsubs,length(soas));
+    end
+    
+    isoa = 0;
+    for this_soa = soas
+        isoa = isoa + 1;
+        temp_turn = out_accuracy(out_soa == this_soa & out_angle == 270 & responded & turn_trials);
+        turn_outL(isoa) = sum(temp_turn)/length(temp_turn);
+        clear temp_turn
+        temp_turn = out_accuracy(out_soa == this_soa & out_angle == 90 & responded & turn_trials);
+        turn_outR(isoa) = sum(temp_turn)/length(temp_turn);
+        temp_cont = out_accuracy(out_soa == this_soa & responded & control_trials);
+        control_out(isoa) = sum(temp_cont)/length(temp_cont);
+        clear temp_turn temp_cont
+    end
+
+	% Plot results
+	subplot(widthHeight,widthHeight,i_sub); 
+		plot(soas,turn_outL,'r',soas,turn_outR,'g',soas,control_out,'b'); 
+			legend({'FlexionL','FlexionR','Control'});
+			xlim([min(soas) max(soas)]); 
+			set(gca,'XTick',min(soas):1:max(soas))
+			xlabel('Gabor First < -- SOA (frames) -- > Gabor After')
+			ylabel('Detection Proportion')
+			ylim([.01 1.05])
+			title(current_sub)
+
+	turn_groupL(i_sub,:) = turn_outL;
+    turn_groupR(i_sub,:) = turn_outR;
+	control_group(i_sub,:) = control_out;
+
+end
+
+% Plot Grand Average results
+figure;
+	boundedline(soas, mean(turn_groupL,1), std(turn_groupL,[],1) / sqrt(nsubs),'r',...
+        soas, mean(turn_groupR,1), std(turn_groupR,[],1) / sqrt(nsubs),'g',...
+		soas, mean(control_group,1), std(control_group,[],1) / sqrt(nsubs),'b');
+	legend({'FlexionL','FlexionR','Control'});
+	xlim([min(soas) max(soas)]); 
+	set(gca,'XTick',min(soas):1:max(soas))
+	xlabel('Gabor Change First < ------ SOA (frames) ------ > Gabor Change After')
+	ylabel('Detection Proportion')
+	ylim([.01 1.05])
+	title('Grand Average')    
+    
+    
+    
+    
 
 
