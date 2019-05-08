279314acb9a4bba8fc08919e44fc83396f4f7633
Existentialist-Robot <eredman@ualberta.ca>
2019-05-08 15:41:10 -0600

    Update Orientation_Distance.m

--- Orientation_Distance.m
+++ Orientation_Distance.m
@@ -486,3 +486,23 @@ end
 
 Ori_Diff_SE = std(Cond_Diff_Mean);
 
+%%
+[x,y,z] = peaks;
+z = max(peaks* 100000, 0);
+cmap = [1 1 1; 1 0 0; 1 .5 0; 1 1 0; 0 1 0];
+lbl =  {'0', '1-6400','6400-80000', '80001-500000', '500000+'};
+[n,bin] = histc(z, [0 1 6400 80000 500000 Inf]); 
+pcolor(x,y,bin);
+colormap(cmap);
+
+
+for ii = 1:size(cmap,1)
+    p(ii) = patch(NaN, NaN, cmap(ii,:));
+end
+legend(p, lbl);
+
+
+cb = colorbar;
+set(cb, 'ticks', 1:5, 'ticklabels', lbl);
+set(gca, 'clim', [0.5 5.5]);
+
